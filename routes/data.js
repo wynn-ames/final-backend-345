8e9a4de024cf4926bdaf73016062eb51289fefac
Martin Nester <martinn7352@gmail.com>
2022-04-18 14:31:11 -0400

    update data

--- routes/data.js
+++ routes/data.js
@@ -1,37 +1,36 @@
-const express = require('express');
-const verify = require('./verifyToken')
-const Users = require('../models/Users');
+const express = require("express");
+const verify = require("./verifyToken");
+const Users = require("../models/Users");
 
-const router = express.Router()
+const router = express.Router();
 
-router.get('/',verify, (req, res) => {
+router.get("/", verify, (req, res) => {
+  const data = Users.findById(req.user._id, async (error, data) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(data);
+      res.send(data);
+    }
+  });
+});
 
-    const data = Users.findById(req.user._id, async (error, data) => {
-        if(error) {
-            console.log(error)
-        } else {
-            console.log(data)
-            res.send(data)
-        }
-    })
+router.get("/update", verify, async (req, res) => {
+  let user = await Users.findById(req.user._id);
+  user.data = req.body.data;
+  await user.save();
+  res.send(user);
+});
 
-    
-})
+router.delete("/", verify, (req, res) => {
+  const data = Users.findByIdAndDelete(req.user._id, async (error, data) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(data);
+      res.send(data);
+    }
+  });
+});
 
-router.delete('/',verify, (req, res) => {
-
-    const data = Users.findByIdAndDelete(req.user._id, async (error, data) => {
-        if(error) {
-            console.log(error)
-        } else {
-            console.log(data)
-            res.send(data)
-        }
-    })
-
-    
-})
-
-
-
-module.exports = router
+module.exports = router;

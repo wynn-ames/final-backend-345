6170c435a736f135d9897e5732b8d4ff85a19d2a
Martin Nester <martinn7352@gmail.com>
2022-04-19 18:15:54 -0400

    Enable Cross Origin Resource Sharing

--- app.js
+++ app.js
@@ -1,33 +1,36 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const app = express()
-const bodyParser = require('body-parser')
-require('dotenv/config')
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const bodyParser = require("body-parser");
+require("dotenv/config");
 
-const dataRoute = require('./routes/data')
-const loginRoute = require('./routes/login')
-const registerRoute = require('./routes/register')
-
-app.use(bodyParser.json())
-app.use('/data', dataRoute)
-app.use('/login', loginRoute)
-app.use('/register', registerRoute)
-app.get('/', (req,res) => {
-    res.send('test test 123')
-})
-
+const dataRoute = require("./routes/data");
+const loginRoute = require("./routes/login");
+const registerRoute = require("./routes/register");
+var cors = require("cors");
+app.use(cors());
+app.use(bodyParser.json());
+app.use("/data", dataRoute);
+app.use("/login", loginRoute);
+app.use("/register", registerRoute);
+app.get("/", (req, res) => {
+  res.send("test test 123");
+});
 
 const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.CONNECTION_URL, {
+      useNewUrlParser: true,
+    });
+    console.log("connected to mongodb!!!");
+  } catch (err) {
+    console.log(err);
+  }
+};
 
-    try {
-      await mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true})
-      console.log('connected to mongodb!!!')
-    } catch (err) {
-        console.log(err)
-    }
-    
-}
+connectDB();
 
-connectDB()
-
-app.listen(process.env.PORT || 3000, console.log('listening at ' + process.env.PORT))
+app.listen(
+  process.env.PORT || 3000,
+  console.log("listening at " + process.env.PORT)
+);
